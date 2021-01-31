f2768e771c5a98884c6f0eeeb333eef114ecf9f0
Gilson Guilherme <gilsonguilherme@gmail.com>
2021-01-30 22:02:58 -0300

    Configura porta do Heroku

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
-
-
 mongoose.connect("mongodb+srv://admin:admin-games@cluster0.tj3qe.mongodb.net/dbgames?retryWrites=true&w=majority",
 {
     useNewUrlParser: true,
@@ -17,4 +15,4 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(require("./routes"));
 
-app.listen(3001);
+app.listen(process.env.PORT || 3001);
